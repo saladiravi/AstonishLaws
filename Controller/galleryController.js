59809e86a4c7e7569361ac613d85969bd6671f23
nandakumar999 <tnandakumar@digispheretech.in>
2025-05-14 15:52:09 +0530

    statusCode chnage in vacancies

--- Controller/galleryController.js
+++ Controller/galleryController.js
@@ -183,7 +183,7 @@ exports.getallGallery = async (req, res) => {
         res.status(200).json({
             statusCode: 200,
             message: 'Gallery Fetched Sucessfully',
-            Gallery: allGallery.rows,
+            gallery: allGallery.rows,
         })
     } catch (err) {
         res.status(500).json({ message: 'Internal Server error' })
