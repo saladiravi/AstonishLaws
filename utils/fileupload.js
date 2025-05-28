eff329a03f1f257091b018bf399ce1dda728a61b
nandakumar999 <tnandakumar@digispheretech.in>
2025-05-28 17:28:28 +0530

    mail chnages done

--- utils/fileupload.js
+++ utils/fileupload.js
@@ -15,16 +15,36 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
 }
 
+const allowedMimeTypes = [
+  'image/jpeg',
+  'image/png',
+  'image/jpg',
+  'video/mp4',
+  'video/mpeg',
+  'video/ogg',
+  'video/webm',
+  'video/quicktime',
+  'application/pdf', // ✅ PDF
+  'application/msword', // ✅ .doc
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // ✅ .docx
+  'application/vnd.ms-excel', // ✅ .xls
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' // ✅ .xlsx
+
+];
+
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
     if (!allowedMimeTypes.includes(file.mimetype)) {
-      return cb(new Error('Only images are allowed!'), false);
+      return cb(new Error('Only image and video files are allowed!'), false);
     }
     cb(null, true);
+  },
+  limits: {
+    fileSize: 100 * 1024 * 1024 // optional: limit to 100 MB
   }
 });
+
 
 
 
