1f73e73df004f2ef0a2fa482a2c2d820366022c2
nandakumar999 <tnandakumar@digispheretech.in>
2025-06-16 10:35:25 +0530

    blogs details

--- routes/caseCategoryroutes.js
+++ routes/caseCategoryroutes.js
@@ -1,16 +1,18 @@
-const caseController=require('../Controller/caseCategoryController')
+const blogs=require('../Controller/blogController');
 const express=require('express');
 const router=express.Router();
 const upload=require('../utils/fileupload');
+
+router.post('/addblogs',upload.fields([
+    {name:'blog_image',maxCount:1},{name:'blog_file',maxCount:1}]), blogs.addblog);
  
-router.post('/addcase',upload.fields([
-    {name:'case_image',maxCount:1}]),caseController.addcasecategory);
- 
-router.get('/getallcases',caseController.getallcaseCategory);
-router.get('/getcasesbyId',caseController.getCasesById);
-router.post('/updatecase',upload.fields([
-      {name:'case_image',maxCount:1}]),caseController.updateCase);
-   router.post("/deleteCase",caseController.deleteCase);
- 
- 
+router.get('/getblogs',blogs.getAllBlogs);
+router.post('/getblogByid',blogs.getBlogById);
+router.post('/deleteblog',blogs.deleteBlog);
+router.post('/updateBlog', upload.fields([
+    {name:'blog_image',maxCount:1},{name:'blog_file',maxCount:1}]),blogs.updateBlog);
+router.post('/getBlogbycase',blogs.getBlogsByCaseCategoryId);
+
+
 module.exports=router
+ 
