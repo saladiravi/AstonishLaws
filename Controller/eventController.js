f1d6fd67cf9512fcb80b1fd9bcdb46e71e081918
nandakumar999 <tnandakumar@digispheretech.in>
2025-06-16 11:30:03 +0530

    event added

--- Controller/eventController.js
+++ Controller/eventController.js
@@ -187,7 +187,7 @@ exports.addevent = async (req, res) => {
         // Send success response
         res.status(200).json({
             statusCode: 200,
-            message: 'Added successfully',
+            message: 'Event Added successfully',
             event: {
                 event_id,
                 event_title,
