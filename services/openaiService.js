78800bb68cb0fa0c9a576c44180f78ddb12deddf
nandakumar999 <tnandakumar@digispheretech.in>
2025-07-02 11:55:45 +0530

    latest chnages

--- services/openaiService.js
+++ services/openaiService.js
@@ -2,8 +2,7 @@ const OpenAI = require("openai");
 require("dotenv").config();
 
 const openai = new OpenAI({
-  apiKey: "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"
-
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 module.exports = openai;
